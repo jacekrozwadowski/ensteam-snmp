77150def699c8f404ed6d72bbbadf74015474f71
jacekrozwadowski <jacek.rozwadowski@gmail.com>
2018-03-07 10:36:47 +0100

    Code format

--- src/main/java/com/ensteam/snmp/EnsteamSnmpClientImpl.java
+++ src/main/java/com/ensteam/snmp/EnsteamSnmpClientImpl.java
@@ -29,24 +29,24 @@ import com.ensteam.snmp.exception.MessageErrorException;
 public class EnsteamSnmpClientImpl implements EnsteamSnmpClient {
 
 	private Snmp snmp = null;
-	
+
 	private String address = null;
 	private int snmpVersion = SnmpConstants.version1;
 	private int timeout = 2000;
 	private int retries = 2;
-	
+
 	protected EnsteamSnmpClientImpl(String address, int snmpVersion, int timeout, int retries) {
 		this.address = address;
 		this.snmpVersion = snmpVersion;
 		this.timeout = timeout;
 		this.retries = retries;
-		
-		if(address==null)
+
+		if (address == null)
 			throw new IllegalArgumentException("Empty target address. Please provide it in format udp:<IP>/<Port>");
 	}
-	
-	protected EnsteamSnmpClientImpl(String address){
-		this(address, SnmpConstants.version1, 2000, 2);		
+
+	protected EnsteamSnmpClientImpl(String address) {
+		this(address, SnmpConstants.version1, 2000, 2);
 	}
 
 	@Override
@@ -83,75 +83,80 @@ public class EnsteamSnmpClientImpl implements EnsteamSnmpClient {
 	public void writeToFile(File file, OID oid) throws MessageErrorException, IOException {
 		writeToFile(file, getAsString(oid));
 	}
-	
+
 	@Override
 	public TransportMapping<UdpAddress> init() throws IOException {
 		TransportMapping<UdpAddress> transport = new DefaultUdpTransportMapping();
 		snmp = new Snmp(transport);
-		USM usm = new USM(SecurityProtocols.getInstance(),
-                new OctetString(MPv3.createLocalEngineID()), 0);
+		USM usm = new USM(SecurityProtocols.getInstance(), new OctetString(MPv3.createLocalEngineID()), 0);
 		SecurityModels.getInstance().addSecurityModel(usm);
-		
+
 		transport.listen();
 		return transport;
 	}
-	
+
 	@Override
-	public String getAsString(OID oid) throws MessageErrorException, IOException {		
+	public String getAsString(OID oid) throws MessageErrorException, IOException {
 		PDU pdu = this.get(oid);
-		
-		if(pdu!=null) {
-			if(pdu.size()>0) {
-				if(pdu.getErrorIndex()>0)
+
+		if (pdu != null) {
+			if (pdu.size() > 0) {
+				if (pdu.getErrorIndex() > 0)
 					throw new MessageErrorException(pdu.getErrorStatusText());
 				else
 					return pdu.get(0).getVariable().toString();
 			} else {
 				throw new MessageErrorException("Empty Protocol Data Unit");
 			}
-			
+
 		} else {
 			throw new MessageErrorException("Request Timed Out");
 		}
 	}
-	
+
 	@Override
 	public void getAsync(OID oid, ResponseListener listener) throws IOException {
 		getAsync(new OID[] { oid }, listener);
 	}
-	
-	/* Write content to file with try-with-resources Statement  */
+
+	/* Write content to file with try-with-resources Statement */
 	private void writeToFile(File file, String text) throws IOException {
-		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
-		    bufferedWriter.write(text);
-		} catch(IOException e){
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+			bufferedWriter.write(text);
+		} catch (IOException e) {
 			throw e;
 		}
 	}
 
 	/* Return Protocol Data Unit for one given Object Identifier Class(OID) */
 	private PDU get(OID oid) throws IOException {
-		if(oid==null)
-			throw new IllegalArgumentException("Empty OID");	
-		
+		if (oid == null)
+			throw new IllegalArgumentException("Empty OID");
+
 		PDU pdu = get(new OID[] { oid });
 		return pdu;
 	}
-	
-	/* Return Protocol Data Unit (PDU) in sync way for set of given Object Identifier Class(OID) */
+
+	/*
+	 * Return Protocol Data Unit (PDU) in sync way for set of given Object
+	 * Identifier Class(OID)
+	 */
 	private PDU get(OID oids[]) throws IOException {
 		PDU pdu = new PDU();
 		for (OID oid : oids) {
 			pdu.add(new VariableBinding(oid));
 		}
 		pdu.setType(PDU.GET);
-		
+
 		PDU res = snmp.send(pdu, getTarget(), null).getResponse();
-		
+
 		return res;
 	}
-	
-	/* Return Protocol Data Unit (PDU) in async way for set of given Object Identifier Class(OID) */
+
+	/*
+	 * Return Protocol Data Unit (PDU) in async way for set of given Object
+	 * Identifier Class(OID)
+	 */
 	private void getAsync(OID oids[], ResponseListener listener) throws IOException {
 		PDU pdu = new PDU();
 		for (OID oid : oids) {
@@ -160,8 +165,8 @@ public class EnsteamSnmpClientImpl implements EnsteamSnmpClient {
 		pdu.setType(PDU.GET);
 		snmp.send(pdu, getTarget(), null, listener);
 	}
-	
-	/* Create Generic Address Target for given address*/
+
+	/* Create Generic Address Target for given address */
 	private Target getTarget() {
 		Address targetAddress = GenericAddress.parse(address);
 		CommunityTarget target = new CommunityTarget();
--- src/main/java/com/ensteam/snmp/exception/MessageErrorException.java
+++ src/main/java/com/ensteam/snmp/exception/MessageErrorException.java
@@ -5,11 +5,11 @@ public class MessageErrorException extends Exception {
 	private static final long serialVersionUID = -1958170128534868182L;
 
 	public MessageErrorException() {
-        super();
-    }
+		super();
+	}
 
-    public MessageErrorException(String msg) {
-        super(msg);
-    }
-	
+	public MessageErrorException(String msg) {
+		super(msg);
+	}
+
 }
